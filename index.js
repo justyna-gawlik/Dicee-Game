201613393a6fb770d1082eeafcf51b9c0a8dec84
Justy <tojajusta8@gmail.com>
2020-10-25 11:21:55 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function play() {
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let image1 = document.querySelector(".img1");
+    let imgName1 = "dice" + randomNumber1 + ".png";
+
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    let image2 = document.querySelector(".img2");
+    let imgName2 = "dice" + randomNumber2 + ".png";
+    image1.setAttribute("src", imgName1);
+    image2.setAttribute("src", imgName2);
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "☜ Player 1 Wins!";
+    } else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "☞ Player 2 Wins!";
+    } else {
+        document.querySelector("h1").innerHTML = "☟ Draw!";
+    }
+};
